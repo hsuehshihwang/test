b61ce68fa594cfb7e2804253dd3804ab763e510e
Ralph Wang <enhanceralph@gmail.com>
2025-03-20 14:48:34 +0800

    update

--- wifi/mac80211_hwsim/wpa2/ap_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# docker run -d --name wifi-ap --privileged --network=none -v .:/work -e ROLE=AP wifi-container 
-docker run --rm --name wifi-ap --privileged --network=none -v .:/work -e ROLE=AP -it wifi-container bash
-# docker run --rm --name wifi-ap --privileged --network=host -v .:/work -e ROLE=AP -it wifi-container bash
-
--- wifi/mac80211_hwsim/wpa2/dnsmasq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ip addr del 192.168.150.1/24 dev wlan0
-ip addr add 192.168.150.1/24 dev wlan0
-kill -9 $(cat /var/run/dnsmasq_ns1.pid)
-dnsmasq -C ./dnsmasq.conf
-
